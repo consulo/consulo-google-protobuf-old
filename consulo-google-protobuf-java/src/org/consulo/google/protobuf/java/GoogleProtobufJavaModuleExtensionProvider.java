7cedc86369d9bdf34053940495b50fcfec5674bc
VISTALL <vistall.valeriy@gmail.com>
2014-01-16 21:13:40 +0200

    compilation fixed

--- consulo-google-protobuf-java/src/org/consulo/google/protobuf/java/GoogleProtobufJavaModuleExtensionProvider.java
+++ consulo-google-protobuf-java/src/org/consulo/google/protobuf/java/GoogleProtobufJavaModuleExtensionProvider.java
@@ -1,20 +1,14 @@
 package org.consulo.google.protobuf.java;
 
-import com.intellij.openapi.module.Module;
 import org.consulo.google.protobuf.module.extension.GoogleProtobufModuleExtensionProvider;
 import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.module.Module;
 
 /**
  * @author VISTALL
  * @since 06.07.13.
  */
 public class GoogleProtobufJavaModuleExtensionProvider extends GoogleProtobufModuleExtensionProvider<GoogleProtobufJavaModuleExtension, GoogleProtobufJavaMutableModuleExtension> {
-  @NotNull
-  @Override
-  public Class<GoogleProtobufJavaModuleExtension> getImmutableClass() {
-    return GoogleProtobufJavaModuleExtension.class;
-  }
-
   @NotNull
   @Override
   public GoogleProtobufJavaModuleExtension createImmutable(@NotNull String s, @NotNull Module module) {

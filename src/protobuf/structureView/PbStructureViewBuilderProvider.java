d6dfc9d55f8f57b80d9451346d1f26aced60f7ec
VISTALL <vistall.valeriy@gmail.com>
2014-02-08 23:36:31 +0200

    compilation fixed

--- src/protobuf/structureView/PbStructureViewBuilderProvider.java
+++ src/protobuf/structureView/PbStructureViewBuilderProvider.java
@@ -1,24 +1,29 @@
 package protobuf.structureView;
 
+import org.jetbrains.annotations.NotNull;
 import com.intellij.ide.structureView.StructureViewBuilder;
 import com.intellij.ide.structureView.StructureViewModel;
 import com.intellij.ide.structureView.TreeBasedStructureViewBuilder;
 import com.intellij.lang.PsiStructureViewFactory;
+import com.intellij.openapi.editor.Editor;
 import com.intellij.psi.PsiFile;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * @author Nikolay Matveev
  */
-public class PbStructureViewBuilderProvider implements PsiStructureViewFactory {
-    @Override
-    public StructureViewBuilder getStructureViewBuilder(final PsiFile psiFile) {
-        return new TreeBasedStructureViewBuilder() {
-            @NotNull
-            @Override
-            public StructureViewModel createStructureViewModel() {
-                return new PbStructureViewModel(psiFile);
-            }
-        };
-    }
+public class PbStructureViewBuilderProvider implements PsiStructureViewFactory
+{
+	@Override
+	public StructureViewBuilder getStructureViewBuilder(final PsiFile psiFile)
+	{
+		return new TreeBasedStructureViewBuilder()
+		{
+			@NotNull
+			@Override
+			public StructureViewModel createStructureViewModel(Editor editor)
+			{
+				return new PbStructureViewModel(psiFile);
+			}
+		};
+	}
 }
